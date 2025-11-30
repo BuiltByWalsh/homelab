89123248a68cc16c86621c98f3112e38607220bb
James Walsh <jamesandersonwalsh@gmail.com>
2025-11-30 00:39:34 -0700

    refactor: use relative paths in scripts

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -2,6 +2,6 @@
 
 # --- Stop docker processes ---
 echo "Shutting down docker services..."
-docker compose down
+docker compose -f ../docker-compose.yml down
 echo "Docker services have been stopped. Shutting down homelab..."
 # ---
